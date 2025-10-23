bd787c3b6452b8016c6f3251dc5abca4ff309544
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2025-10-23 23:29:22 +0300

    strip VERIFY

--- src/supp/verify.h
+++ src/supp/verify.h
@@ -3,25 +3,24 @@
 #define _STRINGIZE2(x) #x
 #define _STRINGIZE1(x) _STRINGIZE2(x)
 
-#include <logging/log.h>     // LFATAL
+#include <logging/log.h>  // LFATAL
 
 #include <cstdlib>  // abort
 
-#define _VERIFY(expr, ...)                                                     \
-    [&] {                                                                      \
-        auto&& retval__ = (expr);                                              \
-        if (!static_cast<bool>(retval__)) [[unlikely]] {                       \
-            LFATAL(                                                            \
-                "Verify failed: loc=" __FILE__                                 \
-                ":" _STRINGIZE1(__LINE__) ", expr='" #expr "': " __VA_ARGS__); \
-            abort();                                                           \
-        }                                                                      \
-        return retval__;                                                       \
+#define _VERIFY(expr, ...)                                             \
+    [&] {                                                              \
+        auto&& retval__ = (expr);                                      \
+        if (!static_cast<bool>(retval__)) [[unlikely]] {               \
+            LFATAL("Verify failed expr='" #expr "': ", ##__VA_ARGS__); \
+            abort();                                                   \
+        }                                                              \
+        return retval__;                                               \
     }()
 
 #define VERIFY(...) _VERIFY(__VA_ARGS__)
 
 #if defined(DEBUG) || defined(SUPP_FORCE_DASSERT)
+
 #define DASSERT(expr, ...) (void)_VERIFY(expr, ##__VA_ARGS__)
 
 #else
